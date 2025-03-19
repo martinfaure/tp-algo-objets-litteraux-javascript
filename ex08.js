32bc54d56296a37e636e8e2a6017ee35d50a1b74
Martin Faure <127661359+martinfaure@users.noreply.github.com>
2025-03-19 11:46:21 +0100

    commit change ex08.js

--- ex08.js
+++ ex08.js
@@ -1,7 +1,12 @@
 function afficherProprietes(obj) {
 	let a = "";
-	a += obj.nom + obj.age + obj.ville;
-	return a;
+	for (const [key, value] of Object.entries(obj)) {
+		a += `${key}: ${value}` + ", ";
+	}
+	let b = a.split("");
+	b.pop();
+	b.pop();
+	return b.join("");
 }
 
 console.log(afficherProprietes({ nom: "Alice", age: 30, ville: "Paris" })); // 'nom: Alice, age: 30, ville: Paris'
